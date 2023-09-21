ff89aa186b8694e4e78a9680ddba91e006970caf
NightPleasure <barbaian.victor@gmail.com>
2023-09-21 14:08:00 +0300

    try-catch

--- Publisher/Publisher.py
+++ Publisher/Publisher.py
@@ -3,33 +3,47 @@ import json
 
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("192.168.30.222", 5050))
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(("170.0.0.1", 5050))
 
-    client.sendall("publisher".encode('utf-8'))
+        client.sendall("publisher".encode('utf-8'))
 
-    send_message(client)
-    client.close()
+        send_message(client)
+        client.close()
+
+    except socket.error as e:
+        print(f"Eroare la conexiunea cu socket-ul: {e}")
+    except Exception as e:
+        print(f"Eroare: {e}")
 
 
 def send_message(client):
-    while True:
-        topic = input("Introduceți topicul: ")
-        title = input("Introduceți titlul postarii: ")
-        content = input("Introduceți conținutul postarii: ")
+    try:
+        while True:
+            topic = input("Introduceți topicul: ")
+            title = input("Introduceți titlul postarii: ")
+            content = input("Introduceți conținutul postarii: ")
 
-        message = {
-            "topic": topic,
-            "title": title,
-            "message": content
-        }
+            message = {
+                "topic": topic,
+                "title": title,
+                "message": content
+            }
 
-        serialized_message = json.dumps(message)
-        client.sendall(serialized_message.encode('utf-8'))
+            serialized_message = json.dumps(message)
+            client.sendall(serialized_message.encode('utf-8'))
 
-        response = input("Doriti sa mai faceti o postare?: \n 1. Da \n 2. Nu \n")
-        if response != "1":
-            break
+            response = input("Doriti sa mai faceti o postare?: \n 1. Da \n 2. Nu \n")
+            if response != "1":
+                break
+                
+    except json.JSONDecodeError:
+        print("Eroare la serializarea mesajului.")
+    except socket.error as e:
+        print(f"Eroare la trimiterea mesajului prin socket: {e}")
+    except Exception as e:
+        print(f"Eroare: {e}")
 
 
 if __name__ == "__main__":
