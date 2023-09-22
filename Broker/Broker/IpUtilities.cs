cd50195d868bda5c4669af852e6234eb84f05ea2
1Leomas <81852464+1Leomas@users.noreply.github.com>
2023-09-22 14:36:56 +0300

    get automatically ip and port (#6)

---
+++ Broker/Broker/IpUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Broker;   
+
+public class IpUtilities
+{
+    public static IPAddress GetIpAddress()
+    {
+        var ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+        return ipEntry.AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork);
+    }
+
+    public static int GetAvailablePort()
+    {
+        var udp = new UdpClient(0, AddressFamily.InterNetwork);
+        return ((IPEndPoint)udp.Client.LocalEndPoint).Port;
+    }
+}   
