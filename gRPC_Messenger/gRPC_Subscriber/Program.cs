802c3ae7dffd578b8dbaae06353c76559b1350c9
A13x <69859268+saneag@users.noreply.github.com>
2023-10-04 15:00:23 +0300

    add subscriber to project (#13)
    Co-authored-by: Sam Rutcovschi <crutcovschi@gmail.com>

--- gRPC_Messenger/gRPC_Subscriber/Program.cs
+++ gRPC_Messenger/gRPC_Subscriber/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Grpc.Net.Client;
+using gRPC_Common;
+using gRPC_Messenger;
+using gRPC_Subscriber.Services;
+using Spectre.Console;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.Urls.Add(EndPoints.Subscriber);
+
+app.MapGrpcService<NotificationService>();
+
+app.MapGet("/", () => "");
+
+app.Start();
+
+Console.Clear();
+
+var appAddress = app.Urls.First();
+
+var httpHandler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+
+Console.Write("Enter broker ip: ");
+var brokerIp = Console.ReadLine();
+
+if (brokerIp == null)
+{
+    brokerIp = EndPoints.Broker;
+}
+else
+{
+    brokerIp = $"https://{brokerIp}:5050";
+}
+
+using var channel = GrpcChannel.ForAddress(brokerIp,
+    new GrpcChannelOptions { HttpHandler = httpHandler });
+
+var client = new Subscriber.SubscriberClient(channel);
+
+var topics = AnsiConsole.Prompt(
+    new MultiSelectionPrompt<string>()
+        .Title("Select topics")
+        .PageSize(10)
+        .AddChoices(new[]
+        {
+            "sport",
+            "politics",
+            "business",
+            "entertainment",
+            "science",
+            "health",
+            "technology",
+            "world"
+        })
+);
+
+foreach (string topic in topics)
+{
+    AnsiConsole.WriteLine(topic);
+}
+
+var request = new SubscribeRequest()
+{
+    Address = appAddress,
+    Subscriptions = { topics }
+};
+
+try
+{
+    var reply = await client.SubscribeAsync(request);
+    if (!reply.IsSuccess)
+    {
+        Console.WriteLine($"Error subscribing");
+        return;
+    }
+
+    Console.WriteLine($"You are now subscribed!");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error subscribing: {e.Message}");
+}
+
+Console.ReadKey();
