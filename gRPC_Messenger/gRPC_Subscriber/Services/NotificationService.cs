4dcf471321940e43efcaf6f31900a20cb18e0153
Sam Rutcovschi <crutcovschi@gmail.com>
2023-10-04 11:18:21 +0300

    add subscriber to project

---
+++ gRPC_Messenger/gRPC_Subscriber/Services/NotificationService.cs
@@ -0,0 +1,17 @@
+﻿using gRPC_Messenger;
+using Grpc.Core;
+
+namespace gRPC_Subscriber.Services;
+
+public class NotificationService : Notification.NotificationBase
+{
+    public override Task<NotifyReply> Notify(NotifyRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"Notification received: {request.Title} {request.Message}");
+
+        return Task.FromResult(new NotifyReply
+        {
+            IsSuccess = true
+        });
+    }
+}   
